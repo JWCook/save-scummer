749adaca28b645b097abe6c009a8b7ddaa810964
Jordan Cook <jordan.cook@pioneer.com>
2021-01-19 11:54:07 -0600

    Add basic commands to list and add games to config

--- save_scummer/config.py
+++ save_scummer/config.py
@@ -1,9 +1,9 @@
 from os import makedirs
 from os.path import isfile, join
-from typing import Dict, Any
+from typing import Any, Dict
 
-from appdirs import user_data_dir
 import yaml
+from appdirs import user_data_dir
 
 DATA_DIR = join(user_data_dir(), 'save-scummer')
 CONFIG_PATH = join(DATA_DIR, 'config.yml')
@@ -11,11 +11,21 @@ BACKUP_DIR = join(DATA_DIR, 'backups')
 DEFAULT_CONFIG = {'games': {}}
 
 
+def add_game(name: str, source_path: str):
+    config = read_config()
+    config['games'][name] = {'source': source_path}
+    write_config(config)
+
+
+# TODO: Add more formatting as more values are added
+def list_games() -> Dict[str, Any]:
+    return read_config()['games']
+
+
 def read_config() -> Dict[str, Any]:
     """Read config from the config file"""
     if not isfile(CONFIG_PATH):
         makedirs(DATA_DIR, exist_ok=True)
-        write_config(DEFAULT_CONFIG)
         return DEFAULT_CONFIG
     with open(CONFIG_PATH) as f:
         return yaml.safe_load(f)
--- save_scummer/main.py
+++ save_scummer/main.py
@@ -1,6 +1,8 @@
+from pprint import pformat
+
 import click
 
-from save_scummer.config import read_config, write_config
+from save_scummer.config import add_game, list_games
 
 
 @click.group()
@@ -9,17 +11,22 @@ def ssc():
 
 
 @ssc.command()
-def add():
-    pass
-
-
-@ssc.command()
-def backup():
-    pass
+@click.argument('name')
+@click.argument('path', type=click.Path(exists=True, resolve_path=True))
+def add(name: str, path: str):
+    """Add a game and its save directory"""
+    add_game(name, path)
+    click.echo(f'Source path for "{name}" added: {path}')
 
 
 @ssc.command()
 def ls():
+    """List all currently configured games"""
+    click.echo(pformat(list_games(), indent=4))
+
+
+@ssc.command()
+def backup():
     pass
 
 
